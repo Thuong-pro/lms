b618d623a0a7749d15826226ec9f3331230c4bef
Thuong pro <dovietthuong2k4@gmail.com>
2026-06-25 13:17:08 +0700

    cập nhật api url

--- Client/src/Helpers/api.js
+++ Client/src/Helpers/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:4000/api/v1';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
